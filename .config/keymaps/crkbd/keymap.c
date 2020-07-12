1718ec346b0e77926289594dd452351b88135d73
Hugo McNally <hugo.mcnally@gmail.com>
2020-07-12 10:36:06 +0100

    keymap: more robust layer switching, numpad and alt added

--- .config/keymaps/crkbd/keymap.c
+++ .config/keymaps/crkbd/keymap.c
@@ -15,8 +15,9 @@
 #define LC3_ENT LT(LAY_LC3, KC_ENT)
 
 #define MO_LC6  MO(LAY_LC6)
+#define MO_LC8  MO(LAY_LC8)
 
-#define TG_LC7  TG(LAY_LC7)
+#define DF_LC7  DF(LAY_LC7)
 #define DF_LA1  DF(LAY_LA1)
 #define DF_LI1  DF(LAY_LI1)
 #define DF_LC5  DF(LAY_LC5)
@@ -42,54 +43,55 @@ enum layer_names {
   LAY_LC5, // Common Layer 5 (normal layout like)
   LAY_LC6, // Common Layer 6 (lowered numbers for normal layout like)
   LAY_LC7, // Common Layer 7 (for selecting default layer)
+  LAY_LC8, // Common Layer 8 (numpad)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAY_LI1] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       TG_LC7,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_NUHS,\
+       DF_LC7,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_NUHS,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LGUI,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       SFT_GUI,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_NUBS,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_NO, LI2_TAB, SFT_ESC,    CTL_SPC, LC3_ENT,   KC_NO \
+                                           MO_LC8, LI2_TAB, SFT_ESC,    CTL_SPC, LC3_ENT, KC_LALT \
                                       //`--------------------------'  `--------------------------'
   ),
 
   [LAY_LI2] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_GRV,   UK_AT, KC_AMPR, KC_LBRC, KC_RBRC, UK_PIPE,                      KC_PAST,    KC_7,    KC_8,    KC_9, KC_CIRC, UK_EURO,\
+       KC_GRV,   UK_AT, KC_EXLM, KC_LBRC, KC_RBRC, UK_PIPE,                      KC_ASTR,    KC_7,    KC_8,    KC_9, KC_MINS, UK_EURO,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LGUI, KC_PMNS, KC_UNDS, KC_LPRN, KC_RPRN, UK_DQUO,                      KC_PPLS,    KC_4,    KC_5,    KC_6, KC_PEQL,  KC_DLR,\
+      KC_LGUI, KC_AMPR, KC_UNDS, KC_LPRN, KC_RPRN, UK_DQUO,                      KC_PLUS,    KC_4,    KC_5,    KC_6,  KC_EQL,  KC_DLR,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      SFT_GUI,   KC_LT,   KC_GT, CURLY_L, CURLY_R, KC_EXLM,                         KC_0,    KC_1,    KC_2,    KC_3, KC_PERC,  UK_PND,\
+      SFT_GUI,   KC_LT,   KC_GT, CURLY_L, CURLY_R, KC_CIRC,                         KC_0,    KC_1,    KC_2,    KC_3, KC_PERC,  UK_PND,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_NO, KC_TRNS, KC_LSFT,    CTL_SPC, KC_BSPC,   KC_NO \
+                                           MO_LC8, KC_TRNS, KC_LSFT,    CTL_SPC, KC_BSPC, KC_LALT \
                                       //`--------------------------'  `--------------------------'
   ),
 
   [LAY_LA1] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       TG_LC7,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_HASH,\
+       DF_LC7,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_HASH,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LGUI,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       SFT_GUI,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_NO, LA2_TAB, SFT_ESC,    CTL_SPC, LC3_ENT,   KC_NO \
+                                           MO_LC8, LA2_TAB, SFT_ESC,    CTL_SPC, LC3_ENT, KC_LALT \
                                       //`--------------------------'  `--------------------------'
   ),
 
   [LAY_LA2] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_GRV,   KC_AT, KC_AMPR, KC_LBRC, KC_RBRC, KC_PIPE,                      KC_PAST,    KC_7,    KC_8,    KC_9, KC_CIRC, KC_TILD,\
+       KC_GRV,   KC_AT, KC_EXLM, KC_LBRC, KC_RBRC, KC_PIPE,                      KC_ASTR,    KC_7,    KC_8,    KC_9, KC_MINS, KC_TILD,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LGUI, KC_PMNS, KC_UNDS, KC_LPRN, KC_RPRN, KC_DQUO,                      KC_PPLS,    KC_4,    KC_5,    KC_6, KC_PEQL,  KC_DLR,\
+      KC_LGUI, KC_AMPR, KC_UNDS, KC_LPRN, KC_RPRN, KC_DQUO,                      KC_PLUS,    KC_4,    KC_5,    KC_6,  KC_EQL,  KC_DLR,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      SFT_GUI,   KC_LT,   KC_GT, CURLY_L, CURLY_R, KC_EXLM,                         KC_0,    KC_1,    KC_2,    KC_3, KC_PERC,   KC_NO,\
+      SFT_GUI,   KC_LT,   KC_GT, CURLY_L, CURLY_R, KC_CIRC,                         KC_0,    KC_1,    KC_2,    KC_3, KC_PERC,   KC_NO,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_NO, KC_TRNS, KC_LSFT,    CTL_SPC, KC_BSPC,   KC_NO \
+                                           MO_LC8, KC_TRNS, KC_LSFT,    CTL_SPC, KC_BSPC, KC_LALT \
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -101,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       SFT_GUI,   KC_NO,   KC_F1,   KC_F2,   KC_F3,  KC_F10,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  KC_INS, CTL_ALT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_NO,  KC_ESC,  KC_TAB,     KC_SPC, KC_TRNS,   KC_NO \
+                                           MO_LC8,  KC_ESC,  KC_TAB,     KC_SPC, KC_TRNS, KC_LALT \
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -113,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LCTL,  MO_LC6,  KC_SPC,     KC_SPC,  KC_ENT,  TG_LC7 \
+                                          KC_LCTL,  MO_LC6,  KC_SPC,     KC_SPC,  KC_ENT,  DF_LC7 \
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -131,14 +133,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAY_LC7] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,\
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_NO,  DF_LI1,  DF_LA1,  DF_LC5,   KC_NO,   KC_NO,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO, KC_TRNS \
+                                            KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO \
                                       //`--------------------------'  `--------------------------'
   ),
 
+  [LAY_LC8] = LAYOUT( \
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_PAST,   KC_P7,   KC_P8,   KC_P9, KC_PMNS, KC_NLCK,\
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                      KC_PPLS,   KC_P4,   KC_P5,   KC_P6, KC_PEQL, KC_PCMM,\
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_P0,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, KC_PDOT,\
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_TRNS,   KC_NO,   KC_NO,     KC_SPC, KC_PENT,   KC_NO \
+                                      //`--------------------------'  `--------------------------'
+  ),
 };
