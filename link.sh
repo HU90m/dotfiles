2f7f18ee1ea7b6831115c322b6728a8be9d42153
Hugo McNally <hugo.mcnally@gmail.com>
2020-09-28 19:08:59 +0100

    link.sh: script which places links in the configs' locations

---
+++ link.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# places links in the configs' locations
+
+pushd ~
+ln -vs ~1/.inputrc
+ln -vs ~1/.bashrc
+ln -vs ~1/.vimrc
+
+cd ~/.config
+ln -vs ~1/.config/picom
+ln -vs ~1/.config/i3
