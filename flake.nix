52147dfd26c62158b78f18fe9500b7c6e95984c2
Hugo McNally <hugo.mcnally@gmail.com>
2025-01-13 14:41:04 +0000

    nix: updated to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "HU90m's Personal Packages, Shells and Machines";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     lowrisc-it = {
--- mod/workstation.nix
+++ mod/workstation.nix
@@ -83,7 +83,6 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -142,7 +141,7 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
   };
-  hardware.opengl.driSupport32Bit = true;
+  hardware.graphics.enable32Bit = true;
 
   # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
@@ -167,7 +166,7 @@
 
   fonts.packages = with pkgs; [
     noto-fonts
-    noto-fonts-cjk
+    noto-fonts-cjk-sans
     noto-fonts-emoji
     lmodern
     liberation_ttf
