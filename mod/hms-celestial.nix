d479f34aa1b39f6bcb7850ce6997d8c3efb52eea
Hugo McNally <hugo.mcnally@gmail.com>
2026-08-31 09:49:02 +0100

    nixos: update to 26.05

--- mod/hms-celestial.nix
+++ mod/hms-celestial.nix
@@ -30,6 +30,7 @@
   ];
 
   boot.zfs.extraPools = [];
+  boot.zfs.forceImportRoot = false;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
