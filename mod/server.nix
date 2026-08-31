abd32569db2d8cb148a96e7bbcb5230c5a35c1c4
Hugo McNally <hugo.mcnally@gmail.com>
2026-08-31 10:17:56 +0100

    nix: update nextcloud to 34

--- mod/server.nix
+++ mod/server.nix
@@ -140,7 +140,7 @@
 
     nextcloud = {
       enable = true;
-      package = pkgs.nextcloud33;
+      package = pkgs.nextcloud34;
       hostName = "cloud.hugom.uk";
       https = true;
       datadir = "/mnt/storage/cloud";
